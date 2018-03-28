31e4c436857b2515e77aca71a66e0b0ac2ccae0f
R0m1nh0 <romualdoosfilho@hotmail.com>
2018-03-28 05:47:13 -0300

    Criando steroid shmup 1

---
+++ Life of a tetris/Assets/Scripts/ShmupAsteroidScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShmupAsteroidScript : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	private float _rotation;
+	private float speedX;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		speedX = Random.Range (-1f, 1f);
+		_rotation = Random.Range (-1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		rb2d.velocity = new Vector3( speedX, -0.5f, 0f);
+		transform.Rotate (0f, 0f, _rotation * 90f * Time.deltaTime);
+	}
+}
